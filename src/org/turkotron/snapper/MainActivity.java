f2b7715b9cf837ac9ab8399f8d5124d6baffda74
Vincent Velociter <vincent.velociter@gmail.com>
2014-06-27 15:43:22 +0200

    Launch camera

--- src/org/turkotron/snapper/MainActivity.java
+++ src/org/turkotron/snapper/MainActivity.java
@@ -1,133 +1,124 @@
 package org.turkotron.snapper;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Camera;
-import android.hardware.Camera.AutoFocusCallback;
-import android.hardware.Camera.PictureCallback;
-import android.hardware.Camera.ShutterCallback;
 import android.os.Bundle;
+import android.hardware.Camera;
 import android.util.Log;
+import android.hardware.Camera.PictureCallback;
+import android.widget.FrameLayout;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.view.ViewGroup.LayoutParams;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.FrameLayout;
+import android.os.Environment;
 
-public class MainActivity extends Activity {
-	private static final String TAG = "MainActivity";
-	Preview preview;
-	Button buttonClick;
-	Camera camera;
-	String fileName;
-	Activity act;
-	Context ctx;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends Activity
+{
+    private static final String TAG = "MainActivity";
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
 
-		super.onCreate(savedInstanceState);
-		// ctx = this;
-		// act = this;
-		// requestWindowFeature(Window.FEATURE_NO_TITLE);
-		// getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		setContentView(R.layout.main);
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
 
-		// preview = new Preview(this, (SurfaceView)findViewById(R.id.surfaceView));
-		// preview.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-		// ((FrameLayout) findViewById(R.id.preview)).addView(preview);
-		// preview.setKeepScreenOn(true);
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
 
-		// buttonClick = (Button) findViewById(R.id.buttonClick);
+        // take picture
+        // mCamera.takePicture(null, null, mPicture);
+    }
 
-		// buttonClick.setOnClickListener(new OnClickListener() {
-		// 	public void onClick(View v) {
-		// 		//				preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-		// 		camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-		// 	}
-		// });
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
 
-		// buttonClick.setOnLongClickListener(new OnLongClickListener(){
-		// 	@Override
-		// 	public boolean onLongClick(View arg0) {
-		// 		camera.autoFocus(new AutoFocusCallback(){
-		// 			@Override
-		// 			public void onAutoFocus(boolean arg0, Camera arg1) {
-		// 				//camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-		// 			}
-		// 		});
-		// 		return true;
-		// 	}
-		// });
+    private PictureCallback mPicture = new PictureCallback() {
 
-	}
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		//      preview.camera = Camera.open();
-		camera = Camera.open();
-		camera.startPreview();
-		preview.setCamera(camera);
-	}
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
 
-	@Override
-	protected void onPause() {
-		if(camera != null) {
-			camera.stopPreview();
-			preview.setCamera(null);
-			camera.release();
-			camera = null;
-		}
-		super.onPause();
-	}
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
 
-	private void resetCam() {
-		camera.startPreview();
-		preview.setCamera(camera);
-	}
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
 
-	ShutterCallback shutterCallback = new ShutterCallback() {
-		public void onShutter() {
-			// Log.d(TAG, "onShutter'd");
-		}
-	};
+        Log.d("MyCameraApp",
+                "Ext storage state:" + Environment.getExternalStorageState());
+        File mediaStorageDir = new File(
+                Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "CameraTest");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
 
-	PictureCallback rawCallback = new PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera camera) {
-			// Log.d(TAG, "onPictureTaken - raw");
-		}
-	};
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp",
+                        "failed to create directory " + mediaStorageDir.getAbsolutePath());
+                return null;
+            }
+        }
 
-	PictureCallback jpegCallback = new PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera camera) {
-			FileOutputStream outStream = null;
-			try {
-				// Write to SD Card
-				fileName = String.format("/sdcard/camtest/%d.jpg", System.currentTimeMillis());
-				outStream = new FileOutputStream(fileName);
-				outStream.write(data);
-				outStream.close();
-				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+            .format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            Log.d("MyCameraApp", mediaStorageDir.getPath() + File.separator + "IMG_"
+                    + timeStamp + ".jpg");
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"
+                    + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_"
+                    + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
 
-				resetCam();
-
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-			}
-			Log.d(TAG, "onPictureTaken - jpeg");
-		}
-	};
+        return mediaFile;
+    }
 }
